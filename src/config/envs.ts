091c4ece07e66bc3ad2e16d1a6b5215ad6ae99a4
Adrian Costa <adriancosta1215@gmail.com>
2026-07-05 08:19:14 -0300

    adding docker and fixing tests

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,14 +1,17 @@
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
 
 export function loadEnv() {
-  const path =
+  const envFiles =
     process.env.NODE_ENV === "test"
-      ? ".env.test"
+      ? [".env.test", ".env"]
       : process.env.NODE_ENV === "development"
-        ? ".env.development"
-        : ".env";
+        ? [".env.development", ".env"]
+        : [".env"];
 
-  const currentEnvs = dotenv.config({ path });
+  const selectedEnvFile = envFiles.find((envFile) => fs.existsSync(path.resolve(envFile)));
+  const currentEnvs = dotenv.config({ path: selectedEnvFile ?? ".env" });
   dotenvExpand.expand(currentEnvs);
 }
